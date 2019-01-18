61648d871d5425a9b60b49f7d5ff93c20e3a2d49
Audun <auduntre@student.matnat.uio.no>
2019-01-18 18:17:19 +0100

    week1

--- exercises/week1/temp_measure.c
+++ exercises/week1/temp_measure.c
@@ -26,22 +26,51 @@ int main (int argc, char **argv)
     int *minute = (int *) malloc (line_count * sizeof(int));
 
     double avg_temp = 0.0;
+    double max_temp, min_temp;
+    double std_dev = 0.0;
+    int max_idx = 0;
+    int min_idx = 0;
+
 
     if (infile == NULL) {
         printf("Not able to open file. \n");
         return 1;
     }
+        
+    getline(&fileline, &line_length, infile);
+    sscanf(fileline, "%d:%d  %lf", &hour[0], &minute[0],  &temps[0]);
     
-    for (int i = 0; i < line_count; i++) {
+    max_temp = temps[0];
+    min_temp = temps[0];
+    
+    for (int i = 1; i < line_count; i++) {
         getline(&fileline, &line_length, infile);
         sscanf(fileline, "%d:%d  %lf", &hour[i], &minute[i],  &temps[i]);
 
+        if (max_temp < temps[i]) {
+            max_temp = temps[i];
+            max_idx = i;
+        } 
+        else if (min_temp > temps[i]) {
+            min_temp = temps[i];
+            min_idx = i;
+        }
+
         avg_temp += temps[i];
     }
-
     avg_temp /= line_count;
 
-    printf("Average Temperature was %f degrees. \n", avg_temp);
+    for (int i = 1; i < line_count; i++) {
+        std_dev += (temps[i] - avg_temp) * (temps[i] - avg_temp);
+    }
+    std_dev /= line_count - 1;
+
+    printf("Average Temperature was %f with standard deviation of %f. \n",
+           avg_temp, std_dev);
+    printf("Highest Teperature was %f at %d:%d. \n", max_temp, hour[max_idx],
+           minute[max_idx]);
+    printf("Lowest Teperature was %f at %d:%d. \n", min_temp, hour[min_idx],
+           minute[min_idx]);
 
     fclose(infile); 
     
