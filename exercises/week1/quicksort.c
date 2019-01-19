845793fcd4877b8d86047cef0f3cb74afb7e644a
Audun <auduntre@student.matnat.uio.no>
2019-01-19 18:20:32 +0100

    week1

--- exercises/week1/quicksort.c
+++ exercises/week1/quicksort.c
@@ -8,9 +8,10 @@ static const int MAX_RAND = RAND_MAX / 10000;
 
 int main (int argc, char **argv)
 {
+    void permution_sort (int arr[], int perm[], int beg, int end);
     void sort (int arr[], int beg, int end);
     void print_array (int arr[], int n);
-    void swap (int *a, int *b);
+    void print_array_perm (int arr[], int perm[], int n);
 
     int n = 100;
 
@@ -20,19 +21,24 @@ int main (int argc, char **argv)
 
     //int *arr = (int *) malloc (n * sizeof(int));
     int arr[n];
+    int arr2[n];
     int perm[n];
 
     srand(time(NULL));
 
     for (int i = 0; i < n; i++) {
         arr[i] = rand() / MAX_RAND;
+        arr2[i] = arr[i];
     }
     
-    print_array(arr, n);
 
+    print_array(arr, n);
     sort(arr, 0, n);
-
     print_array(arr, n);
+
+    permution_sort(arr2, perm, 0, n);
+    print_array(arr2, n);
+    print_array_perm(arr2, perm, n);
 
     //free(arr);
     return 0;
@@ -61,7 +67,7 @@ void sort (int arr[], int beg, int end)
                 l++;
             }
             else {
-                swap (&arr[l], &arr[--r]);
+                swap(&arr[l], &arr[--r]);
             }
         }
 
@@ -72,11 +78,64 @@ void sort (int arr[], int beg, int end)
 }
 
 
+void permution_sort (int arr[], int perm[], int beg, int end)
+{
+    void sort_perm (int arr[], int perm[], int beg, int end);
+    
+    // INIT PERM MATRIX
+    for (int i = beg; i < end; i++) {
+        perm[i] = i;
+    }
+
+    sort_perm(arr, perm, beg, end);
+}
+
+
+void sort_perm (int arr[], int perm[], int beg, int end)
+{
+    void swap (int *a, int *b);
+
+    if (end > beg + 1) {
+        int piv = arr[perm[beg]];
+        int l = beg + 1;
+        int r = end;
+
+
+        while (l < r) {
+            if (arr[perm[l]] <= piv) {
+                l++;
+            }
+            else {
+                swap(&perm[l], &perm[--r]);
+            }
+
+            swap(&perm[--l], &perm[beg]);
+            sort_perm(arr, perm, beg, l);
+            sort_perm(arr, perm, r, end);
+        }
+    } 
+}
+
+
 void print_array (int arr[], int n)
 {
     printf("-------------------\n");
     for (int i = 0; i < n; i++) {
         printf("%4d, ", arr[i]);
+        
+        if ((i+1) % LINE_CUT == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+
+void print_array_perm (int arr[], int perm[], int n)
+{
+    printf("-------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf("%4d, ", arr[perm[i]]);
         
         if ((i+1) % LINE_CUT == 0) {
             printf("\n");
