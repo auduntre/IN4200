a4e6bd19c82135f0a114976445fed8f95131e32e
Audun <auduntre@student.matnat.uio.no>
2019-01-20 21:50:52 +0100

    week2

--- exercises/week2/read_write_test.c
+++ exercises/week2/read_write_test.c
@@ -3,12 +3,10 @@
 #include <time.h>
 
 
-static const int size_int = sizeof(int);
-
-
 int main (int argc, char *argv[])
 {
     void write_test_files (int n);
+    void read_test_files (int n);
 
     int n = 10000;
 
@@ -21,6 +19,7 @@ int main (int argc, char *argv[])
     srand(time(NULL));
 
     write_test_files(n);
+    read_test_files(n);
 
     return 0;
 }
@@ -33,7 +32,7 @@ void write_test_files (int n)
     FILE *outfile_binary;
     FILE *outfile_ASCII;
 
-    int *random_values = (int *) malloc (n * size_int);
+    int *random_values = (int *) malloc (n * sizeof(int));
 
     for (int i = 0; i < n; i++) {
         random_values[i] = rand();
@@ -45,17 +44,40 @@ void write_test_files (int n)
     outfile_ASCII = fopen("test.txt", "w");
 
     start = clock();
-    fwrite(random_values, size_int, n, outfile_binary);
+    fwrite(random_values, sizeof(int), n, outfile_binary);
     end = clock();
     print_elapsed("write binary", start, end);
 
     start = clock();
-    fwrite(random_values, size_int, n, outfile_ASCII);
+    for (int i = 0; i < n; i++) {
+        fprintf(outfile_ASCII, "%u ", random_values[i]);
+    }
     end = clock();
     print_elapsed("write ASCII", start, end);
-    
+   
+    free(random_values); 
     fclose(outfile_binary);
     fclose(outfile_ASCII);
+}
+
+
+void read_test_files (int n)
+{
+    void print_elapsed (char *what_is_timed, clock_t start, clock_t end);
+
+    FILE *infile_binary;
+    FILE *infile_ASCII;
+
+    int *invalues_binary = (int *) malloc (n * sizeof(int));
+
+    infile_binary = fopen("test.bin", "rb");
+
+    fread(invalues_binary, sizeof(int), n, infile_binary);
+
+    printf("%f", invalues_binary[n-1]);
+
+    free(invalues_binary);
+    fclose(infile_binary);
 }
 
 
