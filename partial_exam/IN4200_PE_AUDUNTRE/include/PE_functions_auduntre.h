de05ebd5699f8979fc026049224db231a7252507
Audun <auduntre@student.matnat.uio.no>
2019-03-14 20:52:10 +0100

    starting documentation

--- partial_exam/IN4200_PE_AUDUNTRE/include/PE_functions_auduntre.h
+++ partial_exam/IN4200_PE_AUDUNTRE/include/PE_functions_auduntre.h
@@ -1,3 +1,9 @@
+/** @file PE_functions_auduntre.h
+ *  @brief Functions for ranking webpages using the PageRank algorithm.
+ *  
+ *  @author Audun Tahina Reitan
+ */
+
 #ifndef PE_FUNCTIONS_AUDUNTRE_H
 #define PE_FUNCTIONS_AUDUNTRE_H
 
@@ -5,6 +11,13 @@ double ** read_grap_from_file (char *filename);
  
 double * PageRank_iterations (int damping, double threshold);
 
+
+/** @breif Listing out the top n websites.
+ *  
+ *  @param PE_score_vector The score vector for webpages.
+ *  @param top_n The number of top sites to list out.
+ *  @return Void  
+ */
 void top_n_webpages (double *PE_score_vector, int top_n);
 
 #endif
