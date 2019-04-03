33a55874b2aafc5a5ac9f6e0fe2126a49e002fc9
Audun Tahina <auduntre@student.matnat.uio.no>
2019-04-03 20:59:19 +0200

    documentation

--- partial_exam/IN4200_PE_AUDUNTRE/src/PE_functions_15301.h
+++ partial_exam/IN4200_PE_AUDUNTRE/src/PE_functions_15301.h
@@ -25,12 +25,25 @@ typedef struct compressed_row_storage CRS;
 
 
 CRS read_graph_from_file (char *filename);
- 
+
+/** FUNCTION: PageRank_iterations
+ * ------------------------------
+ *  Calculates the RageRank score for the sites given by the CRS struct. The
+ *  number of sites is given by crs.len_row_ptr - 1.
+ *
+ *  @param CRS crs: Contains the sparse matrix needed to for calculting each iteration.
+ *                  Also contains the  
+ */
 double * PageRank_iterations (CRS crs, double damping, int maxiter, double threshold);
 
-/** @breif Listing out the top n websites.
- *  
+
+/** FUNCTION: top_n_webpages
+ * ------------------------- 
+ *  Listing and writing the top n websites to the report/output.txt file.
+ *  The PE_score_vector function will be sorted after running this function.
+ *
  *  @param PE_score_vector: The score vector for webpages.
+ *  @param len_n: The length of PE_score_vector.
  *  @param top_n: The number of top sites to list out.
  *  @return Void  
  */
