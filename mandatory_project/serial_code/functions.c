149de117b790ece1022e3418e236a0ca6e005fd2
Audun Tahina <auduntre@student.matnat.uio.no>
2019-04-22 17:26:20 +0200

    compiling

--- mandatory_project/serial_code/functions.c
+++ mandatory_project/serial_code/functions.c
@@ -1,1 +1,36 @@
 /* Functions */
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "functions.h"
+
+
+void allocate_image (image *u, int m, int n)
+{
+    printf("IN HERE\n");
+    return;
+}
+
+
+void deallocate_image (image *u)
+{
+    return;
+}
+
+
+void convert_jpeg_to_image (const unsigned char* image_chars, image *u)
+{
+    return;
+}
+
+
+void convert_image_to_jpeg (const image *u, unsigned char* image_chars)
+{
+    return;
+}
+
+
+void iso_diffusion_denoising (image *u, image *u_bar, float kappa, int iters)
+{
+    return;
+}
--- mandatory_project/serial_code/serial_main.c
+++ mandatory_project/serial_code/serial_main.c
@@ -1,1 +1,57 @@
 /* Serial Main */
+
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "functions.h"
+
+
+void import_JPEG_file (const char *filename, unsigned char **image_chars,
+                       int *image_height, int *image_width,
+                       int *num_components);
+
+void export_JPEG_file (const char *filename, const unsigned char *image_chars,
+                       int image_height, int image_width,
+                       int num_components, int quality);
+
+
+int main(int argc, char **argv)
+{
+    char *input_jpeg_filename, *output_jpeg_filename;
+    image u, u_bar;
+    
+    unsigned char *image_chars;
+    int m, n, c, iters;
+    float kappa;
+    
+    
+    /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
+    if (argc < 5) {
+        kappa = atof(argv[1]);
+        iters = atoi(argv[2]);
+        input_jpeg_filename = argv[3];
+        output_jpeg_filename = argv[4];
+    }
+    else {
+        printf("Error with arguments given at the command line\n");
+        exit(EXIT_FAILURE);
+    }
+
+    
+    import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
+    
+    allocate_image (&u, m, n);
+    allocate_image (&u_bar, m, n);
+    convert_jpeg_to_image (image_chars, &u);
+    
+    iso_diffusion_denoising (&u, &u_bar, kappa, iters);
+    
+    convert_image_to_jpeg (&u_bar, image_chars);
+    export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
+    
+    deallocate_image (&u);
+    deallocate_image (&u_bar);
+    
+    return EXIT_SUCCESS;
+}
