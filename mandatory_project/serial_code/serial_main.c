95717b78108b9cc6f35b2255e5114320a44504e9
Audun Tahina <auduntre@student.matnat.uio.no>
2019-04-22 21:06:43 +0200

    shell script and code fix

--- mandatory_project/serial_code/serial_main.c
+++ mandatory_project/serial_code/serial_main.c
@@ -24,7 +24,7 @@ int main(int argc, char **argv)
     image u, u_bar; 
 
     /* read from command line: kappa, iters, input_jpeg_filename, output_jpeg_filename */
-    if (argc < 5) {
+    if (argc >= 5) {
         kappa = atof(argv[1]);
         iters = atoi(argv[2]);
         input_jpeg_filename = argv[3];
@@ -36,18 +36,28 @@ int main(int argc, char **argv)
     }
 
     import_JPEG_file(input_jpeg_filename, &image_chars, &m, &n, &c);
-    
+    printf("Importing JPEG pictures successfull\n");
+
     allocate_image (&u, m, n);
     allocate_image (&u_bar, m, n);
+    printf("Allocation of image arrays succsessfull\n");
+
     convert_jpeg_to_image (image_chars, &u);
-    
-    iso_diffusion_denoising (&u, &u_bar, kappa, iters);
-    
+    printf("JPEG -> 2D array successfull\n");
+
+    iso_diffusion_denoising (&u, &u_bar, kappa, iters); 
+    printf("Denoising succcessfull\n"); 
+
     convert_image_to_jpeg (&u_bar, image_chars);
+    printf("2D array -> JPEG successfull\n");
+
     export_JPEG_file(output_jpeg_filename, image_chars, m, n, c, 75);
-    
+    printf("Export of JPEG successfull\n");
+
     deallocate_image (&u);
     deallocate_image (&u_bar);
+    printf("Deallocation of image arrays succsessfull\n");
     
     return EXIT_SUCCESS;
 }
+
---
+++ mandatory_project/serial_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+cd serial_code 
+
+input_image=../images/mona_lisa_noisy.jpg
+output_image=../images/mona_lisa_denoised.jpg
+
+time ./serial_main 0.2 1000 $input_image $output_image
+
+cd ..
