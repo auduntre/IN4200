f2705347d7857d5a08cc7b813a3ef8d831ace46a
Audun Tahina <auduntre@student.matnat.uio.no>
2019-05-10 23:20:06 +0200

    documentation

--- mandatory_project/parallel_code/parallel_functions.h
+++ mandatory_project/parallel_code/parallel_functions.h
@@ -3,6 +3,21 @@
 
 #include "functions.h"
 
+/** FUNCTION iso_diffusion_denoising_parallel
+ * ---------------------------------
+ * Parallel isotropic diffusion denoising of an image using MPI with
+ * a cartesian process gird.
+ *
+ * parameters:
+ *   - image *u:           Pointer struct of this process block of the 
+ *                         picture to be denoised.
+ *   - image *u_bar:       Pointer to updated image for each iteration. 
+ *                         The final denoised block of the picture is 
+ *                         in this struct image_data array.
+ *   - float kappa:        Constant values used in the Denoising.
+ *   - int iters:          Number of iterations.
+ *   - MPI_COMM CART_COMM: The cartesian process grid. 
+ */
 void iso_diffusion_denoising_parallel (image *u, image *u_bar, float kappa, 
                                        int iters, MPI_Comm CART_COMM);
 
